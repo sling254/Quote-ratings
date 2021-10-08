e1a848c317cc16c0fe451769db765b4a3d037352
sling254 <mwanikidenis7@gmail.com>
2021-10-08 13:51:40 +0300

    add the quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { QuoteComponent } from './components/quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
