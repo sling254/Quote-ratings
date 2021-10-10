1768408afe407da8b7602032e5383e7f98a67489
sling254 <mwanikidenis7@gmail.com>
2021-10-10 23:18:32 +0300

    Auto-generated commit [ci skip]

--- src/app/highlightquote.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightquoteDirective } from './highlightquote.directive';
-
-describe('HighlightquoteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightquoteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
